7d6c5794392ba9652c393f4d06156461c1f76992
harshani <ruwanthikapemarathna@gmail.com>
2024-03-29 14:09:15 +0530

    connect controller class to the route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//customers is the function name
+Route::get('/api/v1/customers', [CustomerController::class, 'customers']) -> name('');
